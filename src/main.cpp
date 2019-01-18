5c7cc43ebc5520e59be9ee01eee3999a1c370ec2
Henning Makholm <henning@semmle.com>
2019-01-18 19:47:37 +0100

    Add SpookyHash V2

--- src/main.cpp
+++ src/main.cpp
@@ -65,6 +65,8 @@ HashInfo g_hashes[] =
   { SpookyHash32_test,    32, 0x3F798BBB, "Spooky32",    "Bob Jenkins' SpookyHash, 32-bit result" },
   { SpookyHash64_test,    64, 0xA7F955F1, "Spooky64",    "Bob Jenkins' SpookyHash, 64-bit result" },
   { SpookyHash128_test,  128, 0x8D263080, "Spooky128",   "Bob Jenkins' SpookyHash, 128-bit result" },
+  { SpookyHash64V2_test,  64, 0x972C4BDC, "Spooky64V2",  "Bob Jenkins' SpookyHashV2, 64-bit result" },
+  { SpookyHash128V2_test,128, 0x893CFCBE, "SpookyV2",    "Bob Jenkins' SpookyHashV2, 128-bit result" },
 
   // MurmurHash2
 
